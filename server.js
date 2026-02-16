b54daadde196af5809140409afe77b7a06057941
Raj1778 <rajmhr078@gmail.com>
2026-02-16 21:20:30 +0530

    Added ask route for AI response

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDb from "./src/db/db.js";
 import userRouter from "./src/routes/user.routes.js";
 import uploadRouter from "./src/routes/upload.routes.js";
+import qaRoutes from "./src/routes/qa.routes.js";
 
 dotenv.config();
 connectDb();
@@ -11,12 +12,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT;
 app.get("/", (req, res) => {
   res.send("Welcome to Retriev");
 });
 app.use("/api/upload", uploadRouter);
 app.use("/api/user", userRouter);
+app.use("/api", qaRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
