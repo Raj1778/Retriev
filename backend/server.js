cf46979bbc5016a3f85c0a67f2d261ec24d76b78
Raj1778 <rajmhr078@gmail.com>
2026-02-25 22:20:11 +0530

    Resolvd frontend error for deployment

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://retriev.vercel.app/"],
+    origin: ["http://localhost:3000", "https://retriev.vercel.app"],
     credentials: true,
   }),
 );
